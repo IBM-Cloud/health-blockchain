74fb6a0718a140a1fd24bfa561878a3d5e249845
antonm <antonm@ca.ibm.com>
2017-05-08 12:56:22 -0400

    More work on views / health kit

---
+++ phone/fitchain/ChallengeTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  ChallengeTableViewCell.swift
+//  fitchain
+//
+//  Created by Anton McConville on 2017-05-08.
+//  Copyright © 2017 Anton McConville. All rights reserved.
+//
+
+import UIKit
+
+class ChallengeTableViewCell: UITableViewCell {
+    
+//    @IBOutlet weak var hotelIcon: UIImageView!
+//    @IBOutlet weak var hotelLabel: UILabel!
+//    @IBOutlet weak var endLabel: UILabel!
+//    @IBOutlet weak var beginLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
+
+
