8c6fc90e11439e0c9af51df9c9d50466ea796b23
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2017-05-22 11:11:39 +0200

    allow to override app hostname

--- .bluemix/pipeline-DEPLOY.sh
+++ .bluemix/pipeline-DEPLOY.sh
@@ -9,7 +9,11 @@ cf create-service compose-for-mongodb Standard health-blockchain-db
 # Push app with blue/green deployment
 ################################################################
 if ! cf app $CF_APP; then
-  cf push $CF_APP
+  if [ -z "$CF_APP_HOSTNAME" ]; then
+    cf push $CF_APP
+  else
+    cf push $CF_APP --hostname $CF_APP_HOSTNAME
+  fi
 else
   OLD_CF_APP=${CF_APP}-OLD-$(date +"%s")
   rollback() {
@@ -24,6 +28,10 @@ else
   set -e
   trap rollback ERR
   cf rename $CF_APP $OLD_CF_APP
-  cf push $CF_APP
+  if [ -z "$CF_APP_HOSTNAME" ]; then
+    cf push $CF_APP
+  else
+    cf push $CF_APP --hostname $CF_APP_HOSTNAME
+  fi
   cf delete $OLD_CF_APP -f
 fi
