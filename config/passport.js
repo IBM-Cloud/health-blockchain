78bec8c1ee6109c8c21f11c05b7489c1114667ef
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2017-05-22 15:31:27 +0200

    passport implementation with Cloudant

--- config/passport.js
+++ config/passport.js
@@ -1,143 +1,140 @@
-// config/passport.js
-
-// load all the things we need
 var LocalStrategy = require('passport-local').Strategy;
 
 // load up the user model
-var User = require('../models/account');
+var dba = "account";
+var Account;
 
+function initCloudant(appEnv) {
+  var cloudantURL = appEnv.services.cloudantNoSQLDB[0].credentials.url || appEnv.getServiceCreds("health-blockchain-db").url;
+  var Cloudant = require('cloudant')({url: cloudantURL, plugin: 'retry', retryAttempts: 10, retryTimeout: 500});
 
-function createPolicies(account) {
-
+  // Create the accounts DB if it doesn't exist
+  Cloudant.db.create(dba, function(err, body) {
+    if (err) {
+      console.log("Database already exists: ", dba);
+    } else {
+      console.log("New database created: ", dba);
+    }
+  });
+  Account = Cloudant.use(dba);
 }
 
+// =====================================
+// EXPORT LOGIN & SIGNUP ===============
+// =====================================
+module.exports = function(passport, appEnv) {
+  initCloudant(appEnv);
 
-// expose this function to our app using module.exports
-module.exports = function (passport) {
+  var bcrypt = require('bcrypt-nodejs');
 
-    // =========================================================================
-    // passport session setup ==================================================
-    // =========================================================================
-    // required for persistent login sessions
-    // passport needs ability to serialize and unserialize users out of session
+  // =========================================================================
+  // passport session setup ==================================================
+  // =========================================================================
+  // required for persistent login sessions
+  // passport needs ability to serialize and unserialize users out of session
 
-    // used to serialize the user for the session
-    passport.serializeUser(function (user, done) {
-        done(null, user.id);
+  // used to serialize the user for the session
+  passport.serializeUser(function(user, done) {
+    done(null, user.email);
+  });
+
+  // used to deserialize the user
+  passport.deserializeUser(function(email, done) {
+    Account.find({
+      selector: {
+        email
+      }
+    }, function(err, result) {
+      if (err) {
+        return done(err);
+      }
+      var user = result.docs[0];
+      done(null, user);
     });
+  });
 
-    // used to deserialize the user
-    passport.deserializeUser(function (id, done) {
-        User.findById(id, function (err, user) {
-            done(err, user);
-        });
+  passport.use('local-login', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  }, function(req, email, password, done) {
+
+    console.log("Got login request");
+
+    // Use Cloudant query to find the user
+    Account.find({
+      selector: {
+        email
+      }
+    }, function(err, result) {
+      if (err) {
+        console.log("There was an error finding the user: " + err);
+        return done(null, null, err);
+      }
+      if (result.docs.length === 0) {
+        console.log("Email was not found");
+        return done(null, false, "Email or password incorrect.");
+      }
+
+      // user was found, now determine if password matches
+      var user = result.docs[0];
+      if (bcrypt.compareSync(password, user.password)) {
+        console.log("Password matches");
+        return done(null, user, null);
+      } else {
+        console.log("Password is not correct");
+        return done(null, null, "Email or password incorrect.");
+      }
     });
+  }));
 
-    // =========================================================================
-    // LOCAL SIGNUP ============================================================
-    // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
+  // =========================================================================
+  // LOCAL SIGNUP ============================================================
+  // =========================================================================
+  // we are using named strategies since we have one for login and one for signup
+  // by default, if there was no name, it would just be called 'local'
+  passport.use('local-signup', new LocalStrategy({
+    usernameField: 'email', passwordField: 'password', passReqToCallback: true // allows us to pass back the entire request to the callback
+  }, function(req, email, password, done) {
+    console.log('Signup for: ', email);
 
-    passport.use('local-signup', new LocalStrategy({
-            // by default, local strategy uses username and password, we will override with email
-            usernameField: 'email',
-            passwordField: 'password',
-            passReqToCallback: true // allows us to pass back the entire request to the callback
-        },
-        function (req, email, password, done) {
+    var firstName = req.body.fname;
+    var lastName = req.body.lname;
 
-            console.log('signup');
+    // Use Cloudant query to find the user just based on user name
+    Account.find({
+      selector: {
+        'email': email
+      }
+    }, function(err, result) {
+      if (err) {
+        console.log("There was an error registering the user: " + err);
+        return done(null, null, err);
+      } else if (result.docs.length > 0) {
+        console.log("Email was found");
+        return done(null, null, "User already exists. Use another email address.");
+      }
 
-            console.log('passport.js email:' + email);
+      // create the new user
+      var hash_pass = bcrypt.hashSync(password);
+      var user = {
+        "_id": email,
+        email,
+        "password": hash_pass,
+        "first_name": firstName,
+        "last_name": lastName
+      };
 
-            // asynchronous
-            // User.findOne wont fire unless data is sent back
-            process.nextTick(function () {
+      Account.insert(user, function(err, body) {
+        if (err) {
+          console.log("There was an error registering the user: " + err);
+          return done(null, null, err);
+        } else {
+          console.log("User successfully registered.");
+          return done(null, user, null);
+        }
+      });
+    });
+  }));
 
-                // find a user whose email is the same as the forms email
-                // we are checking to see if the user trying to login already exists
-                User.findOne({
-                    'local.email': email
-                }, function (err, user) {
-                    // if there are any errors, return the error
-                    if (err) {
-                        console.log('signup error');
-                        return done(err);
-                    }
-
-                    // check to see if theres already a user with that email
-                    if (user) {
-                        console.log('That email is already taken');
-                        return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-                    } else {
-
-                        console.log('attempting to make an account');
-
-                        // if there is no user with that email
-                        // create the user
-                        var newUser = new User();
-
-
-                        // set the user's local credentials
-                        newUser.local.email = email;
-                        newUser.local.password = newUser.generateHash(password);
-
-                        // save the user
-                        newUser.save(function (err) {
-                            if (err) {
-                                throw err;
-                            }
-
-                            return done(null, newUser);
-                        });
-
-                        console.log('made an account');
-                    }
-
-                });
-
-            });
-
-        }));
-
-    passport.use('local-login', new LocalStrategy({
-            // by default, local strategy uses username and password, we will override with email
-            usernameField: 'email',
-            passwordField: 'password',
-            passReqToCallback: true // allows us to pass back the entire request to the callback
-        },
-        function (req, email, password, done) { // callback with email and password from our form
-
-            console.log('local login');
-
-            // find a user whose email is the same as the forms email
-            // we are checking to see if the user trying to login already exists
-            User.findOne({
-                'local.email': email
-            }, function (err, user) {
-                // if there are any errors, return the error before anything else
-                if (err) {
-                    return done(err);
-                }
-
-                // if no user is found, return the message
-                if (!user) {
-                    console.log('user not found');
-                    return done(null, false, req.flash('loginMessage', 'No user found.'));
-                } // req.flash is the way to set flashdata using connect-flash
-
-                // if the user is found but the password is wrong
-                if (!user.validPassword(password)) {
-
-                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
-                } // create the loginMessage and save it to session as flashdata
-
-                // all is well, return successful user
-
-                console.log('successful login');
-                return done(null, user);
-            });
-
-        }));
 };
