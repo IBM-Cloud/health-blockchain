af93d987bf6ac71deb56b2c839694f4d9146cabb
Ram Vennam <rvennam987@gmail.com>
2017-05-30 23:18:08 -0500

    Add login

--- ui-react/src/Phone/Phone.js
+++ ui-react/src/Phone/Phone.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
 import './Phone.css';
 
+
 class Phone extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: 'rvennam@us.ibm.com',
+      password: 'password',
+      errorMessage: ''
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.login = this.login.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
   login() {
-    console.log('login', this);
+    fetch('/login', {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify({
+        email: this.state.email, password: this.state.password
+      })
+    }).then((response) => {
+      console.log(response);
+      if (response.ok) {
+        return response;
+      }
+      this.setState({ errorMessage: 'Network response was not ok.' });
+      return response;
+    });
   }
   render() {
     return (
@@ -26,6 +65,8 @@ class Phone extends Component {
                     id="email"
                     className="logininput" type="text" name="email"
                     placeholder="E-mail address"
+                    onChange={this.handleInputChange}
+                    value={this.state.email}
                   />
                 </div>
                 <div className="loginfield">
@@ -34,6 +75,8 @@ class Phone extends Component {
                     id="password" className="logininput"
                     type="password" name="password"
                     placeholder="Password"
+                    onChange={this.handleInputChange}
+                    value={this.state.password}
                   />
                 </div>
                 <button className="loginbutton" onClick={this.login}>Login</button>
@@ -41,7 +84,9 @@ class Phone extends Component {
                   <a className="signuplink" href="./signup">Add a new account</a>
                 </div>
               </div>
-              <div className="messagearea" id="messagearea" />
+              <div className="messagearea" id="messagearea">
+                {this.state.errorMessage}
+              </div>
             </div>
           </div>
 
