8c2f650b61971d05f0fd599a2bb98b5d3b827e88
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2017-07-12 11:16:15 +0200

    latest 5.2 npm does not install dependencies https://github.com/npm/npm/issues/16839 sticking with the version shipping with node 6.9.1

--- .bluemix/pipeline-BUILD.sh
+++ .bluemix/pipeline-BUILD.sh
@@ -4,7 +4,6 @@ npm config delete prefix
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.2/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 6.9.1
-npm install npm@latest -g
 
 # build the React app
 cd ui-react
